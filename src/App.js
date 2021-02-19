1ff848dfff901372c6042c7c1cdf9438e84dd41f
Andriy <lobatskiy31@gmail.com>
2021-02-19 15:11:23 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [parentUrl, setParentUrl] = useState(
+    "http://127.0.0.1:5500/index.html"
+  );
+
+  function onclick() {
+    const testObj = [
+      { id: 123456, quantity: 2 },
+      { id: 78910, quantity: 1 },
+    ];
+
+    var parent = window.parent;
+    parent.postMessage(JSON.stringify(testObj), parentUrl);
+    console.log("send date parent page");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <div className="title"> Test "postMessage" Iframe </div>
+
+        <div>
+          <label className="box_Title">Parent window url</label>
+          <div>
+            <input
+              className="input"
+              type="text"
+              name="parent/url"
+              onChange={(event) => setParentUrl(event.target.value)}
+              value={parentUrl}
+            ></input>
+          </div>
+        </div>
+
+        <br />
+        <button className="btn" onClick={() => onclick()}>
+          Send Data
+        </button>
+      </div>
+    </>
   );
 }
 
